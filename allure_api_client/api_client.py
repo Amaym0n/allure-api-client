e93c0aafc44b372bd957314d6d97ded978c71616
amaym0n <loco.skitls@gmail.com>
2023-12-30 14:44:59 +0300

    Renamed JWTToken to BearerToken. Fixed poetry dependencies

--- allure_api_client/api_client.py
+++ allure_api_client/api_client.py
@@ -37,16 +37,16 @@ def response_hook(response: Response) -> None:
     return
 
 
-class JWTToken(Auth):
-    def __init__(self, jwt_token: str) -> None:
-        self.jwt_token = jwt_token
-        if not self.jwt_token:
-            raise Exception("jwt_token is mandatory.")
+class BearerToken(Auth):
+    def __init__(self, token: str) -> None:
+        self.token = token
+        if not self.token:
+            raise Exception("The token is mandatory")
 
     def auth_flow(
             self, request: Request
     ) -> Generator[Request, Response, None]:
-        request.headers['Authorization'] = f'Bearer {self.jwt_token}'
+        request.headers['Authorization'] = f'Bearer {self.token}'
         yield request
 
 
