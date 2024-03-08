a6acf8bf4c41db267697405821abb43978af7a8d
amaym0n <loco.skitls@gmail.com>
2024-03-08 21:20:45 +0300

    Fixed curl and updated version

--- allure_api_client/hooks.py
+++ allure_api_client/hooks.py
@@ -1,5 +1,3 @@
-import json
-
 import allure
 from httpx import Response, Request
 
@@ -18,10 +16,12 @@ def request_hook(request: Request) -> None:
         client = httpx.Client(event_hooks={'request': [request_hook]})
     """
     with allure.step(title=f'Request: [{request.method}] --> {request.url}'):
-        headers = request.headers
+        headers = []
+        for header in request.headers:
+            headers.append(f'-H "{header}: {request.headers[header]}"')
         body = '' if request.content == b'' \
             else f" --data '{request.content if isinstance(request.content, str) else request.content.decode()}'"
-        curl = f"curl --location '{request.url}' --header '{json.dumps(dict(headers))}'{body}"
+        curl = f"curl --location '{request.url}' {' '.join(headers)}{body}"
         print(curl)
         allure.attach(curl, 'request', allure.attachment_type.TEXT)
     return
